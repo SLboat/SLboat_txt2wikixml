abb1b54d32b666f5c23de85c63eca3e6f4b94ef1
Sen <slboat@me.com>
2013-06-10 23:38:47 +0800

    不必要的时候不写文件

--- txt2wiki.pl
+++ txt2wiki.pl
@@ -11,7 +11,7 @@
 ## 支持中文文件-非常重要-是的完全支持
 ## title支持过滤xml字符
 ## 添加每次提交信息为某次提交
-
+## 一种参数模式，整合丢弃整理文件到一个子目录 --move
 
 use HTML::Entities;
 
@@ -20,7 +20,7 @@ my $indir = `pwd`;
 # 测试模式-加上本地地址
 # $indir = '/Users/sen/tools/txt2wiki/xml_text/';
 
-print "\t森亮号见识整合水手\t\n";
+print "\n\t森亮号见识整合水手\t\n";
 
 # 这是做了啥-去除了换行字符等东西
 chomp $indir;
@@ -32,16 +32,10 @@ my $comment = '来自TextMate的见识建造!由mediawiki2wiki.pl生产xml';
 my $counts = 0;
 # 最终固定输出文件名
 $outfile = "SLboat_SEEING.xml";
-# 提醒开始干活
-print "\n开始整合见识文件...\n\n";
 
 # 先打开了目录-看起来是
 opendir(INDIR, $indir) || die "couldn't open $indir for reading";
 
-# 打开输出流，这里很像管道呢
-open(OUT, ">$outfile") || die "couldn't open $outfile for write";
-# 输出头部
-&printxmlheader();
 # 读取检查每一个文件
 while($infile = readdir(INDIR)) {
 	# 没有后缀[.]除掉
@@ -52,7 +46,15 @@ while($infile = readdir(INDIR)) {
 	#反向死亡,死亡是全部退出这很糟糕-或许只需要退出while就够了
 	# 判断是否有效的待转换格式文件
 	next unless ($infile =~ m/mediawiki$/);
-	
+	# 文件有效
+	if ($counts==0){ # 首次写头部
+		# 提醒开始干活
+		print "\n开始整合见识文件...\n\n";
+		# 打开输出流，这里很像管道呢
+		open(OUT, ">$outfile") || die "couldn't open $outfile for write";
+		# 输出头部
+		&printxmlheader();
+	}
 	# 截取标题
 	$title = $infile;
 	# s模式替换源字符串为空白
@@ -76,14 +78,19 @@ while($infile = readdir(INDIR)) {
 	# 好了加一也不错
 	$counts++;
 }
-# 输出尾巴部分
-&printxmlfooter();
-# 最终关闭文件
-close(OUT);
+# todo：也许可以判断直接输出流是否有效，但它没有null吧
+if ($counts>0){ # 首次写头部
+	# 输出尾巴部分
+	&printxmlfooter();
+	# 最终关闭文件
+	close(OUT);
+	# 告别
+	print "\n整合见识完毕!这次一共整合了$counts个见识文件\n它们被放置于: $outfile\n\n";
+	}else{
+		print "\n没有任何可以进行整合的文件！真是让人遗憾！\n\n"
+}
 # 关闭目录-为啥要关闭呢
 closedir(INDIR);
-# 告别
-print "\n整合见识完毕!这次一共整合了$counts个见识文件\n它们被放置于: $outfile\n\n";
 
 # todo:使用函数一样的玩意改写-page::head
 # page的额头-不能有换行
